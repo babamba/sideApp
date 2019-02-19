9410fec08d788f056c45b038559e040d59c36039
starlord <starlord@xhiftcorp.com>
2019-02-19 18:46:04 +0900

    모달팝업 붙이기

--- components/AddButton/index.js
+++ components/AddButton/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
-import { TouchableOpacity,TouchableHighlight, View, StyleSheet, Text } from "react-native";
+import { TouchableOpacity,TouchableHighlight, View, StyleSheet, Text ,ScrollView , Dimensions} from "react-native";
 import { withNavigation } from "react-navigation";
 import Modal from "react-native-modal";
 
@@ -18,9 +18,25 @@ class AddButton extends Component {
           console.log("_toggleModal")
           this.setState({ isModalVisible: !this.state.isModalVisible });
      }
+
+     handleOnScroll = event => {
+          this.setState({
+            scrollOffset: event.nativeEvent.contentOffset.y,
+          });
+        };
+      
+     handleScrollTo = p => {
+          if (this.scrollViewRef) {
+            this.scrollViewRef.scrollTo(p);
+          }
+     }
+      
           
      
      render(){
+          const deviceWidth = Dimensions.get("window").width;
+          const deviceHeight = Dimensions.get("window").height;
+
           console.log(this.props);
           return (
                <View>
@@ -28,14 +44,52 @@ class AddButton extends Component {
                     activeOpacity={0.4} 
                     onPress={this._toggleModal}
                     style={styles.button}
-               >
+                    >
                     <View>
                          <Text style={styles.text}>
                               {this.props.AddText}
                          </Text>
                     </View>
-               </TouchableHighlight>
+                    </TouchableHighlight>
 
+                    <Modal 
+                         isVisible={this.state.isModalVisible} 
+                         deviceWidth={deviceWidth}
+                         deviceHeight={deviceHeight}
+                         style={styles.bottomModal}
+                         backdropColor={"grey"}
+                         backdropOpacity={0.9}
+                         onBackdropPress={() => this.setState({ isModalVisible: false })}
+                         onBackButtonPress={() => this.setState({ isModalVisible: false })}
+                         onSwipe={() => this.setState({ isModalVisible: false })}
+                         swipeDirection="down"
+                         onSwipeComplete={() => this.setState({ isModalVisible: false })}
+                         swipeThreshold={10}
+                         
+                         // scrollTo={this.handleScrollTo}
+                         // scrollOffset={this.state.scrollOffset}
+                         // scrollOffsetMax={400 - 300} // content height - ScrollView height
+                    >
+                    {/* <View style={styles.scrollableModal}>
+                         <ScrollView
+                         ref={ref => (this.scrollViewRef = ref)}
+                         onScroll={this.handleOnScroll}
+                         scrollEventThrottle={16}>
+                              <View style={styles.scrollableModalContent1}>
+                                   <Text>Scroll me up</Text>
+                              </View>
+                              <View style={styles.scrollableModalContent1}>
+                                   <Text>Scroll me up</Text>
+                              </View>
+                         </ScrollView>
+                    </View> */}
+
+                    <View style={styles.modalContent}>
+                         <TouchableOpacity onPress={this._toggleModal}>
+                              <Text>Hello!</Text>
+                         </TouchableOpacity>
+                    </View>
+                    </Modal>
                </View>
                
           )
@@ -74,8 +128,24 @@ const styles = StyleSheet.create({
           fontWeight:'900',
           textAlign:'center'
      },
+     modalContent: {
+          backgroundColor: "white",
+          paddingBottom: 620,
+          padding: 22,
+          justifyContent: "center",
+          alignItems: "center",
+          borderRadius: 22,
+          borderColor: "rgba(0, 0, 0, 0.1)",
+          shadowColor: 'gray',
+          shadowOffset: { width: 0, height: 0 },
+          shadowOpacity: 0.3,
+          shadowRadius: 7,
+          elevation: 1,
+     },
      bottomModal: {
           justifyContent: "flex-end",
+          height: 700,
+          borderRadius: 4,
           margin: 0,
      },
 });

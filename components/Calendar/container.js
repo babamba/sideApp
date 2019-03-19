7735b41bb92816e63d3c8a6453dda20be614ce90
starlord <starlord@xhiftcorp.com>
2019-03-19 19:03:09 +0900

    아모르겟당

--- components/Calendar/container.js
+++ components/Calendar/container.js
@@ -16,13 +16,14 @@ LocaleConfig.locales['ko_KR'] = {
 
 LocaleConfig.defaultLocale = 'ko_KR';
 
-export default class Container extends Component {
+class Container extends Component {
      constructor(props) {
           super(props);
           //console.log("calendar : " , props)
           this.state = {
                items: {},
-               refreshing: false
+               refreshing: false,
+               day: null
           };
      }
 
@@ -41,180 +42,203 @@ export default class Container extends Component {
           })
      }
 
-  componentWillReceiveProps = (nextProps) => {
-     
-     if(nextProps){
+     componentWillReceiveProps = async (nextProps) => {
+          if(nextProps){
+               if(nextProps.AllIncreaseData){
+                    console.log("nextProps.AllIncreaseData : ", nextProps.AllIncreaseData)
+                    console.log("state.AllIncreaseData : ", this.state.AllIncreaseData)
+                    await this.setState({
+                         AllIncreaseData : nextProps.AllIncreaseData,
+                    })
+               }
+
+               if(nextProps.AllMealData){
+                    //console.log("nextProps.AllMealData : ", nextProps.AllMealData)
+                    await this.setState({
+                         AllMealData : nextProps.AllMealData,
+                    })
+               }
+
+               if(nextProps.AllPurchaseData){
+                    //console.log("nextProps.AllPurchaseData : ", nextProps.AllPurchaseData)
+                    await this.setState({
+                         AllPurchaseData : nextProps.AllPurchaseData,
+                    })
+               }
+
+               // AllMealData : nextProps.AllMealData,
+               // AllPurchaseData : nextProps.AllPurchaseData,
+
+               //console.log("calendar Component nextProps increaseProduct: " , nextProps.increaseProduct)
+               //console.log("calendar Component nextProps mealProduct: " , nextProps.mealProduct)
+               //console.log("calendar Component nextProps purchaseProduct: " , nextProps.purchaseProduct)
+          
+          
+          }else{
+               console.log("nextProps.NOT : ", nextProps.AllIncreaseData)
+          }
+
+
+          await this.loadItems(this.state.day)
+          await this.renderItem(this.state.items)
 
           this.setState({
-               refreshing : true
-          });
-
-          console.log("nextProps.increaseProduct : ", nextProps.AllIncreaseData)
-          this.setState({
-               AllIncreaseData : nextProps.AllIncreaseData,
-               AllMealData : nextProps.AllMealData,
-               AllPurchaseData : nextProps.AllPurchaseData,
                refreshing : false,
           })
-          //console.log("calendar Component nextProps increaseProduct: " , nextProps.increaseProduct)
-          //console.log("calendar Component nextProps mealProduct: " , nextProps.mealProduct)
-          //console.log("calendar Component nextProps purchaseProduct: " , nextProps.purchaseProduct)
-     }else{
-          console.log("nextProps.NOT : ", nextProps.AllIncreaseData)
-          this.setState({
-               refreshing : false,
-          })
+          // if(increaseProduct.length != this.state.increaseProduct.length
+          //      || mealProduct.length != this.state.mealProduct.length
+          //      || purchaseProduct.length != this.state.purchaseProduct.length
+          //           ){
+          //           this.setState({
+          //                increaseProduct,
+          //                mealProduct,
+          //                purchaseProduct,
+          //                isFetching : false,
+          //           })
+          // }else{
+          //      this.setState({
+          //           isFetching : false,
+          //      })
+          // }
      }
 
-     // if(increaseProduct.length != this.state.increaseProduct.length
-     //      || mealProduct.length != this.state.mealProduct.length
-     //      || purchaseProduct.length != this.state.purchaseProduct.length
-     //           ){
-     //           this.setState({
-     //                increaseProduct,
-     //                mealProduct,
-     //                purchaseProduct,
-     //                isFetching : false,
-     //           })
-     // }else{
-     //      this.setState({
-     //           isFetching : false,
-     //      })
-     // }
-}
-
-  render() {
-     return (
-          <Agenda
-               items={this.state.items}
-               pastScrollRange={3}
-               futureScrollRange={3}
-               loadItemsForMonth={this.loadItems.bind(this)}
-               selected={moment(new Date()).format("YYYY-MM-DD")}
-               renderItem={this.renderItem.bind(this)}
-               renderEmptyDate={this.renderEmptyDate.bind(this)}
-               rowHasChanged={this.rowHasChanged.bind(this)}
-
-               refreshing={this.state.refreshing}
-               onRefresh={this.refresh}
-
-               // refreshControl={
+     render() {
+          return (
+               <Agenda
+                    items={this.state.items}
+                    pastScrollRange={3}
+                    futureScrollRange={3}
+                    loadItemsForMonth={this.loadItems.bind(this)}
+                    selected={moment(new Date()).format("YYYY-MM-DD")}
+                    renderItem={this.renderItem.bind(this)}
+                    renderEmptyDate={this.renderEmptyDate.bind(this)}
+                    rowHasChanged={this.rowHasChanged.bind(this)}
+                    onRefresh={this.refresh.bind(this)}
+                    refreshing={this.state.refreshing}
+                    refreshControl = {null}
+                    //onDayPress={(day)=>{  } }
+                    onDayPress={(day)=>{ this.changeDay(day) }}
                     
+                    // refreshControl={
+                         
+                         
+                    // }
                     
-               // }
-               
-               //markingType={'period'}
-               // markedDates={{
-               //      '2019-03-19': {color: 'red'},
-               //      '2019-03-19': {endingDay: true, color: 'gray'}
-               // }}
-               // monthFormat={'yyyy'}
-               // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
-               //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
-          />
-     );
-  }
-
-  filterProduct = (strTime) => {
-       const { increaseProduct , mealProduct, purchaseProduct}  = this.state;
-       const {AllIncreaseData, AllMealData, AllPurchaseData} = this.state
-     // increaseProduct
-     // mealProduct 
-     // purchaseProduct
-     //console.log("strTime : " , strTime);
-     //console.log(this.state.increaseProduct);
-     //console.log(this.state.mealProduct);
-     //console.log(this.state.purchaseProduct);
-
-     let count = 0;
-
-     let time = strTime;
-     let convert_strTime = moment(time).format("YYYY-MM-DD")
-
-     let dataObj = {};
-     dataObj.increase = [];
-     dataObj.meal = [];
-     dataObj.purchase = [];
-
-     //console.log("())()()()()()()() increaseProduct", increaseProduct)
-      if(typeof AllIncreaseData != "undefined"){
-          if(AllIncreaseData.length > 0){
-               let obj = AllIncreaseData;
-               
-
-               for (let j = 0; j < Object.keys(obj).length; j++) {
-                    let inner = obj[j];
-
-                    let createTime = inner.created_at;
-                    let convert_createTime = moment(createTime).format("YYYY-MM-DD")
-
-                    //console.log("#_)#_#_#_#_#_#_#__increase strTime : ",convert_strTime , " / createTime : " , convert_createTime)
-                    
-                    if(convert_strTime === convert_createTime){
-                         //count++;
-                         dataObj.increase.push({ time : convert_createTime, color:"#55efc4", price : inner.price , consumType: "수입" , income_name : inner.income_name});
-                         //console.log("#_)#_#_#_#_#_#_#_increase strTime createTime same time" , convert_strTime , " / " , convert_createTime)
-                    }
-                    //MonthMealPrice += price;
-               }
-          }
-          //console.log("dataobj.meal : " , dataObj.increase)
+                    //markingType={'period'}
+                    // markedDates={{
+                    //      '2019-03-19': {color: 'red'},
+                    //      '2019-03-19': {endingDay: true, color: 'gray'}
+                    // }}
+                    // monthFormat={'yyyy'}
+                    // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
+                    //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
+               />
+          );
      }
 
-     if(typeof AllMealData != "undefined" ){
-          if(AllMealData.length > 0){
-               let obj = AllMealData;
-               
+     filterProduct = (strTime) => {
+          const { increaseProduct , mealProduct, purchaseProduct}  = this.state;
+          const {AllIncreaseData, AllMealData, AllPurchaseData} = this.state
+          // increaseProduct
+          // mealProduct 
+          // purchaseProduct
+          //console.log("strTime : " , strTime);
+          //console.log(this.state.increaseProduct);
+          //console.log(this.state.mealProduct);
+          //console.log(this.state.purchaseProduct);
 
-               //console.log("mealProduct : ", obj)
-               for (let j = 0; j < Object.keys(obj).length; j++) {
-                    let inner = obj[j];
+          let count = 0;
 
-                    let createTime = inner.created_at;
-                    let convert_createTime = moment(createTime).format("YYYY-MM-DD")
+          let time = strTime;
+          let convert_strTime = moment(time).format("YYYY-MM-DD")
 
-                    if(convert_strTime === convert_createTime){
-                         dataObj.meal.push({time : convert_createTime, color:"#74b9ff", price : inner.price , consumType: "식사", income_name : inner.income_name});
+          let dataObj = {};
+          dataObj.increase = [];
+          dataObj.meal = [];
+          dataObj.purchase = [];
+
+          //console.log("())()()()()()()() increaseProduct", increaseProduct)
+          if(typeof AllIncreaseData != "undefined"){
+               if(AllIncreaseData.length > 0){
+                    let obj = AllIncreaseData;
+                    
+
+                    for (let j = 0; j < Object.keys(obj).length; j++) {
+                         let inner = obj[j];
+
+                         let createTime = inner.created_at;
+                         let convert_createTime = moment(createTime).format("YYYY-MM-DD")
+
+                         //console.log("#_)#_#_#_#_#_#_#__increase strTime : ",convert_strTime , " / createTime : " , convert_createTime)
+                         
+                         if(convert_strTime === convert_createTime){
+                              //count++;
+                              dataObj.increase.push({ time : convert_createTime, color:"#55efc4", price : inner.price , consumType: "수입" , income_name : inner.income_name});
+                              //console.log("#_)#_#_#_#_#_#_#_increase strTime createTime same time" , convert_strTime , " / " , convert_createTime)
+                         }
+                         //MonthMealPrice += price;
                     }
                }
+               //console.log("dataobj.meal : " , dataObj.increase)
           }
-          //console.log("dataobj.meal : " , dataObj.meal)
-     }
 
-     if( typeof AllPurchaseData != "undefined"){
-          if(AllPurchaseData.length > 0){
-               let obj = AllPurchaseData;
-              
-
-               for (let j = 0; j < Object.keys(obj).length; j++) {
-                    let inner = obj[j];
-
-                    let createTime = inner.created_at;
-                    let convert_createTime = moment(createTime).format("YYYY-MM-DD")
-
-                    //console.log("#_)#_#_#_#_#_#_#_ meal strTime : ",convert_strTime , " / createTime : " , convert_createTime)
+          if(typeof AllMealData != "undefined" ){
+               if(AllMealData.length > 0){
+                    let obj = AllMealData;
                     
-                    if(convert_strTime === convert_createTime){
-                         //count++;
-                         dataObj.purchase.push({ time : convert_createTime, color:"#ff7675", price : inner.price , consumType: "소비" , income_name : inner.income_name});
-                         //console.log("#_)#_#_#_#_#_#_#_ meal strTime createTime same time" , convert_strTime , " / " , convert_createTime)
+
+                    //console.log("mealProduct : ", obj)
+                    for (let j = 0; j < Object.keys(obj).length; j++) {
+                         let inner = obj[j];
+
+                         let createTime = inner.created_at;
+                         let convert_createTime = moment(createTime).format("YYYY-MM-DD")
+
+                         if(convert_strTime === convert_createTime){
+                              dataObj.meal.push({time : convert_createTime, color:"#74b9ff", price : inner.price , consumType: "식사", income_name : inner.income_name});
+                         }
                     }
-                    //MonthMealPrice += price;s
                }
+               //console.log("dataobj.meal : " , dataObj.meal)
           }
 
-          //console.log("dataobj.purchase : " , dataObj.purchase)
+          if( typeof AllPurchaseData != "undefined"){
+               if(AllPurchaseData.length > 0){
+                    let obj = AllPurchaseData;
+               
+
+                    for (let j = 0; j < Object.keys(obj).length; j++) {
+                         let inner = obj[j];
+
+                         let createTime = inner.created_at;
+                         let convert_createTime = moment(createTime).format("YYYY-MM-DD")
+
+                         //console.log("#_)#_#_#_#_#_#_#_ meal strTime : ",convert_strTime , " / createTime : " , convert_createTime)
+                         
+                         if(convert_strTime === convert_createTime){
+                              //count++;
+                              dataObj.purchase.push({ time : convert_createTime, color:"#ff7675", price : inner.price , consumType: "소비" , income_name : inner.income_name});
+                              //console.log("#_)#_#_#_#_#_#_#_ meal strTime createTime same time" , convert_strTime , " / " , convert_createTime)
+                         }
+                         //MonthMealPrice += price;s
+                    }
+               }
+
+               //console.log("dataobj.purchase : " , dataObj.purchase)
+          }
+
+          //console.log("filter end : " , count)
+
+          //dataObj.count = count
+          
+
+          return dataObj;
      }
-
-     //console.log("filter end : " , count)
-
-     //dataObj.count = count
-     
-
-     return dataObj;
-  }
 
      loadItems(day) {
+          this.setState({
+               day : day
+          })
           setTimeout(() => {
                //console.log("day", day)
 
@@ -299,56 +323,68 @@ export default class Container extends Component {
           }, 1000);
           // console.log(`Load Items for ${day.year}-${day.month}`);
      }
+     changeDay = (day) => {
+          console.log("changeDay", day)
+          this.setState({
+               day : day
+          })
 
-  renderItem(item) {
-    return (
-      //<View style={[styles.item, {height: item.height}]}>
+          console.log("current state day" , this.state.day)
+     }
 
-               <View style={[styles.item, 
-                    {
-                         backgroundColor: item.color,
-                         shadowColor: item.color,
-                         shadowOffset: { width: 0, height: 0 },
-                         shadowOpacity: 0.5,
-                         shadowRadius: 6,
+     renderItem(item) {
+          console.log("render item")
+          return (
+          //<View style={[styles.item, {height: item.height}]}>
+
+                    <View style={[styles.item, 
+                         {
+                              backgroundColor: item.color,
+                              shadowColor: item.color,
+                              shadowOffset: { width: 0, height: 0 },
+                              shadowOpacity: 0.5,
+                              shadowRadius: 6,
+                         
+                         }]}>
                     
-                    }]}>
-                   
-                         <Text style={styles.incomeText}>{item.income_name}</Text>
+                              <Text style={styles.incomeText}>{item.income_name}</Text>
 
-                         <Text style={styles.priceText}>{item.price}</Text>
-                         <Text style={styles.won}>원</Text>
-                         <Text style={styles.typeText}>{item.name}</Text>
-                    {/* <Text>{item.time}</Text> */}
-                    
-               </View>
-    );
-  }
+                              <Text style={styles.priceText}>{item.price}</Text>
+                              <Text style={styles.won}>원</Text>
+                              <Text style={styles.typeText}>{item.name}</Text>
+                         {/* <Text>{item.time}</Text> */}
+                         
+                    </View>
+          );
+     }
 
-  refresh = async() => {
-       const { getAllData } = this.props;
+     refresh = async() => {
+          const { getAllData } = this.props;
+          console.log("isFetch refresh")
+          this.setState({
+               refreshing : true
+          });
 
-       await getAllData(moment().format("YYYYMMDD"), 0);
-       await getAllData(moment().format("YYYYMMDD"), 1);
-       await getAllData(moment().format("YYYYMMDD"), 2);  
-       
-       console.log("isFetch refresh")
-  }
 
-  renderEmptyDate() {
-    return (
-      <View style={styles.emptyDate}><Text>기록이 없습니다.</Text></View>
-    );
-  }
+          await getAllData(moment().format("YYYYMMDD"), 0);
+          await getAllData(moment().format("YYYYMMDD"), 1);
+          await getAllData(moment().format("YYYYMMDD"), 2);  
+     }
 
-  rowHasChanged(r1, r2) {
-    return r1.name !== r2.name;
-  }
+     renderEmptyDate() {
+          return (
+               <View style={styles.emptyDate}><Text>기록이 없습니다.</Text></View>
+          );
+     }
 
-  timeToString(time) {
-    const date = new Date(time);
-    return date.toISOString().split('T')[0];
-  }
+     rowHasChanged(r1, r2) {
+          return r1.name !== r2.name;
+     }
+
+     timeToString(time) {
+          const date = new Date(time);
+          return date.toISOString().split('T')[0];
+     }
 }
 
 const styles = StyleSheet.create({
@@ -428,3 +464,5 @@ const styles = StyleSheet.create({
      //      marginTop: 17
      //    }
 });
+
+export default Container

4f04893a39524d0e26f1adf4bf626c509914df7b
starlord <starlord@xhiftcorp.com>
2019-04-01 17:22:11 +0900

    흠..

--- components/CarouselList/entries.js
+++ components/CarouselList/entries.js
@@ -1,18 +1,18 @@
 export const ENTRIES1 = [
      {
-         title: 'Beautiful and dramatic Antelope Canyon',
-         subtitle: 'Lorem ipsum dolor sit amet et nuncat mergitur',
-         illustration: 'https://i.imgur.com/UYiroysl.jpg'
+         title: '아이패드프로',
+         subtitle: '살거야 으아아아아니',
+         illustration: 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/image/AppleInc/aos/published/images/i/pa/ipad/pro/ipad-pro-12-11-select-201810_GEO_KR?wid=870&amp;hei=1100&amp;fmt=jpeg&amp;qlt=95&amp;op_usm=0.5,0.5&amp;.v=1540576022267'
      },
      {
-         title: 'Earlier this morning, NYC',
-         subtitle: 'Lorem ipsum dolor sit amet',
-         illustration: 'https://i.imgur.com/UPrs1EWl.jpg'
+         title: '아이맥',
+         subtitle: '아이맥있으면 좋을것 같은데말이야',
+         illustration: 'https://cdn.clien.net/web/api/file/F01/7642247/afd0e9da5919e.jpg?w=780&h=30000'
      },
      {
-         title: 'White Pocket Sunset',
-         subtitle: 'Lorem ipsum dolor sit amet et nuncat ',
-         illustration: 'https://i.imgur.com/MABUbpDl.jpg'
+         title: '팰리세이드',
+         subtitle: '차도 하나 사고싶어 절약절약',
+         illustration: 'http://www.autodaily.co.kr/news/photo/201812/406447_33632_195.jpg'
      },
  ];
  
